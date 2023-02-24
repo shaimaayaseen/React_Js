d7cc3ef5c120c4201d86d9d5ba592cf84f00eba2
unknown <shaima.alshlouh@gmail.com>
2023-02-24 16:41:56 +0200

    React Js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react"
 // import Home from "./pages/home/Home.jsx";
-import Profile from "./pages/profile/Profile.jsx";
+import Home from "./pages/home/Home.jsx";
 // import Login from "./pages/login/Login.jsx";
 // import Register from "./pages/register/Register.jsx";
 
 function App() {
-  return <Profile/>
+  return <Home/>
 }
 
 export default App;
